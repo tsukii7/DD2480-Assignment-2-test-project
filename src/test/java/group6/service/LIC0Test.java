a4feb4503c0e67e00b0e4ce9ec0bf04d97e82da3
xianqing <1375926565@qq.com>
2025-02-13 12:20:47 +0100

    Update LIC0Test.java

--- src/test/java/group6/service/LIC0Test.java
+++ src/test/java/group6/service/LIC0Test.java
@@ -21,7 +21,7 @@ public class LIC0Test {
         Parameters parameters = new Parameters();
         parameters.setLength1(10);
 
-        assertTrue(licService.evaluateLICById(0, points, parameters), "cmv0 should return true when two consecutive points have a distance greater than LENGTH1");
+        assertFalse(licService.evaluateLICById(0, points, parameters), "cmv0 should return true when two consecutive points have a distance greater than LENGTH1");
     }
 
     @Test
